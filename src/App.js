920af386f3dff2a4b1025599e9cd4d85cc8de5da
Hamza-ABAKI <hamza.abaki@gmail.com>
2021-05-13 17:08:04 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import "./App.css";
-import Profile from "./profile/Profile";
+import './App.css'
+import Profile from './profile/Profile'
 
 function App() {
-  const handleName = (name) => {
-    alert(name);
-  };
-
   return (
-    <div className="App" style={{ margin: "0px 30px", display: "flex" }}>
+    <div
+      className='App'
+      style={{ margin: '0px 30px', display: 'flex', flexDirection: 'column' }}
+    >
+      {/* <Profile /> */}
+
       <Profile
-        fullName={"Sherlock HOLMES"}
+        fullName={'Sherlock HOLMES'}
         bio={
           "Sherlock HOLMES is the fictional but brilliant detective created by Arthur Conan Doyle in 1887, created for the novel A Study In Scarlet, published in Beeton's Christmas Annual. Sherlock Holmes has essentially an obsessive personality. He works compulsively on all his cases and his deductive powers are phenomenal"
         }
-        profession={"Detective"}
-        func={handleName}
+        profession={'Detective'}
       >
         sherlock.jpg
       </Profile>
+
+      <Profile
+        fullName={'John H. WATSON'}
+        bio={
+          'Dr. John H. ... Watson, born in 1852, has served as an army surgeon in India, where he was wounded during the second Afghan War, and has returned to England in impaired health. He and Holmes meet in London; they share rooms at 221B Baker Street.'
+        }
+        profession={'Doctor'}
+      >
+        drWatson.jpg
+      </Profile>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,57 +1,73 @@
-import React from "react";
+import React from 'react'
 
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types'
 
-const Profile = ({ fullName, bio, profession, func, children }) => {
+const Profile = ({ fullName, bio, profession, children }) => {
+  const handleName = (name) => {
+    alert(name)
+  }
+  const style_profile = {
+    margin: '50px',
+    display: 'flex',
+    backgroundColor: 'ivory',
+    borderRadius: '15px',
+    boxShadow:
+      '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+  }
+  const style_txt = {
+    margin: '50px 0px',
+    padding: '30px auto',
+    backgroundColor: 'ivory',
+    maxWidth: '100%',
+  }
+
   return (
-    <div
-      className="Profile"
-      style={{
-        margin: "0px 50px",
-        display: "flex",
-        backgroundColor: "ivory",
-        borderRadius: "15px",
-        boxShadow:
-          "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-      }}
-    >
+    <div className='Profile' style={style_profile}>
       <img
-        style={{ backgroundColor: "ivory", width: "400px" }}
+        style={{ backgroundColor: 'ivory', width: '350px' }}
         src={children}
-        alt=""
+        alt=''
       />
-      <div
-        style={{
-          margin: "50px auto",
-          padding: "30px auto",
-          backgroundColor: "ivory",
-        }}
-      >
-        <h1 style={{ backgroundColor: "ivory" }}>Name: {fullName}</h1>
-        <h2 style={{ backgroundColor: "ivory" }}>Profession: {profession}</h2>
-        <h3 style={{ backgroundColor: "ivory" }}>Biography: </h3>
-        <h5 style={{ backgroundColor: "ivory" }}>{bio}</h5>
+      <div className='txt' style={style_txt}>
+        <h1 style={{ backgroundColor: 'ivory' }}>Name: {fullName}</h1>
+        <h2 style={{ backgroundColor: 'ivory' }}>Profession: {profession}</h2>
+        <h3 style={{ backgroundColor: 'ivory' }}>Biography: </h3>
+        <h5
+          style={{
+            backgroundColor: 'ivory',
+            padding: '15px 20px',
+          }}
+        >
+          {bio}
+        </h5>
+
         <button
-          style={{ backgroundColor: "ivory" }}
-          onClick={() => func(fullName)}
+          style={{
+            backgroundColor: 'ivory',
+            float: 'right',
+            margin: '20px 30px',
+            padding: '10px',
+          }}
+          onClick={() => handleName(fullName)}
         >
           Who I am ?
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
 Profile.defaultProps = {
-  fullName: "Your name",
-  bio: "Describe yourself",
-  profession: "What do you do for living ?",
-  children: "profilePic.jpg",
-};
+  fullName: 'Your name',
+  bio: 'Describe yourself',
+  profession: 'What do you do for living ?',
+  children: 'profilePic.png',
+}
 
 Profile.propTypes = {
   fullName: PropTypes.string,
-  profession: PropTypes.number,
-};
+  profession: PropTypes.string,
+  bio: PropTypes.string,
+}
 
-export default Profile;
+export default Profile
