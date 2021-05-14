a6b564a3e96291c82b64f5a2b8b347e7b7b0dc27
Hamza-ABAKI <hamza.abaki@gmail.com>
2021-05-14 01:27:30 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -2,29 +2,34 @@ import './App.css'
 import Profile from './profile/Profile'
 
 function App() {
+  const handleName = (name) => {
+    alert(name)
+  }
   return (
     <div
       className='App'
       style={{ margin: '0px 30px', display: 'flex', flexDirection: 'column' }}
     >
-      {/* <Profile /> */}
+      <Profile />
 
       <Profile
         fullName={'Sherlock HOLMES'}
+        profession={'Detective'}
         bio={
           "Sherlock HOLMES is the fictional but brilliant detective created by Arthur Conan Doyle in 1887, created for the novel A Study In Scarlet, published in Beeton's Christmas Annual. Sherlock Holmes has essentially an obsessive personality. He works compulsively on all his cases and his deductive powers are phenomenal"
         }
-        profession={'Detective'}
+        func={handleName}
       >
         sherlock.jpg
       </Profile>
 
       <Profile
         fullName={'John H. WATSON'}
+        profession={'Doctor'}
         bio={
           'Dr. John H. ... Watson, born in 1852, has served as an army surgeon in India, where he was wounded during the second Afghan War, and has returned to England in impaired health. He and Holmes meet in London; they share rooms at 221B Baker Street.'
         }
-        profession={'Doctor'}
+        func={handleName}
       >
         drWatson.jpg
       </Profile>
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -2,53 +2,42 @@ import React from 'react'
 
 import PropTypes from 'prop-types'
 
-const Profile = ({ fullName, bio, profession, children }) => {
-  const handleName = (name) => {
-    alert(name)
+const Profile = (props) => {
+  const { fullName, bio, profession, func, children } = props
+
+  const handleClick = () => {
+    alert(fullName)
   }
   const style_profile = {
     margin: '50px',
     display: 'flex',
-    backgroundColor: 'ivory',
     borderRadius: '15px',
     boxShadow:
       '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
   }
   const style_txt = {
-    margin: '50px 0px',
+    margin: '15px 0px',
     padding: '30px auto',
-    backgroundColor: 'ivory',
-    maxWidth: '100%',
+  }
+  const style_button = {
+    float: 'right',
+    margin: '20px 30px',
+    padding: '10px',
   }
 
   return (
     <div className='Profile' style={style_profile}>
-      <img
-        style={{ backgroundColor: 'ivory', width: '350px' }}
-        src={children}
-        alt=''
-      />
+      <img style={{ width: '250px', height: '320px' }} src={children} alt='' />
       <div className='txt' style={style_txt}>
-        <h1 style={{ backgroundColor: 'ivory' }}>Name: {fullName}</h1>
-        <h2 style={{ backgroundColor: 'ivory' }}>Profession: {profession}</h2>
-        <h3 style={{ backgroundColor: 'ivory' }}>Biography: </h3>
-        <h5
-          style={{
-            backgroundColor: 'ivory',
-            padding: '15px 20px',
-          }}
-        >
-          {bio}
-        </h5>
-
+        <h3>Name: {fullName}</h3>
+        <h3>Profession: {profession}</h3>
+        <h3>Biography: </h3>
+        <h5 style={{ padding: '15px 20px' }}>{bio}</h5>
         <button
-          style={{
-            backgroundColor: 'ivory',
-            float: 'right',
-            margin: '20px 30px',
-            padding: '10px',
+          style={style_button}
+          onClick={() => {
+            func === undefined ? handleClick() : func(fullName)
           }}
-          onClick={() => handleName(fullName)}
         >
           Who I am ?
         </button>
@@ -58,8 +47,8 @@ const Profile = ({ fullName, bio, profession, children }) => {
 }
 
 Profile.defaultProps = {
-  fullName: 'Your name',
-  bio: 'Describe yourself',
+  fullName: 'Default name',
+  bio: 'A biography is the non-fiction, written history or account of a person’s life. Biographies are intended to give an objective portrayal of a person, written in the third person. ',
   profession: 'What do you do for living ?',
   children: 'profilePic.png',
 }
